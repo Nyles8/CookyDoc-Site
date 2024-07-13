e38380b8818caa446d59ae1b3a16b265bbc590ed
Nylesssss <2337620979@qq.com>
2024-07-13 19:36:16 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,34 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Cook AI',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '轻松上手',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '基础操作', slug: 'guides/example' },
+						{ label: '使用场景', slug: 'guides/example' },
+						{ label: '模型介绍', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '定价',
+					items: [
+						{ label: '模型消耗', slug: 'reference/example' },
+						{ label: '套餐价格', slug: 'reference/example' },
+						{ label: '无限会员', slug: 'reference/example' },
+					],
+				},
+				{
+					label: '进阶使用',
+					items: [
+						{ label: '提示词技巧', slug: 'reference/example' },
+						{ label: '工作案例', slug: 'reference/example' },
+						{ label: '特殊模型', slug: 'reference/example' },
+					],
 				},
 			],
 		}),
